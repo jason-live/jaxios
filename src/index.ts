db01afadb6cde88107c7e8352a94050c182e9c25
Jason <348842258@qq.com>
2020-01-18 18:32:40 +0800

    feat: :sparkles: 实现 url 处理逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildUrl } from './helpers/url'
 
-function axios(config: AxiosRequestConfig) {
+function axios(config: AxiosRequestConfig): void {
+  processURL(config)
   xhr(config);
+}
+
+function processURL(config: AxiosRequestConfig): void {
+  config.url = transformURL(config);
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildUrl(url, params)
 }
 
 export default axios;
