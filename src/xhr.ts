a5e17c10a7b32ca70b9eb07e9f029bb6fc5bd1c1
Jason <348842258@qq.com>
2020-01-19 10:41:51 +0800

    feat: :sparkles: createError 函数应用 + 导出类型定义

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/header'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
@@ -50,14 +51,14 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
      * 执行网络错误
      */
     request.onerror = function handleError() {
-      reject(new Error('Network Error'))
+      reject(createError('Network Error', config, null, request))
     }
 
     /**
      * 执行超时错误
      */
     request.ontimeout = function handleTimeout() {
-      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+      reject(createError(`Timeout of ${timeout} ms exceeded`, config, 'ECONNABORTED', request))
     }
 
     /**
@@ -89,7 +90,7 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (response.status >= 200 && response.status < 300) {
         resolve(response)
       } else {
-        reject(new Error(`Request failed with status code ${response.status}`))
+        reject(createError(`Request failed with status code ${response.status}`, config, null, request, response))
       }
     }
   })
