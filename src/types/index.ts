b24944eac505e6d7cdc854bd8797f388f6b7236c
Jason <348842258@qq.com>
2020-01-19 11:21:10 +0800

    feat: :sparkles: 声明 Axios 接口

--- src/types/index.ts
+++ src/types/index.ts
@@ -82,3 +82,21 @@ export interface AxiosError extends Error{
   response?: AxiosResponse,
   isAxiosError: boolean
 }
+
+export interface Axios {
+  request(config?: AxiosRequestConfig): AxiosPromise
+
+  get(url: string, config? :AxiosRequestConfig): AxiosPromise
+
+  options(url: string, config? :AxiosRequestConfig): AxiosPromise
+
+  delete(url: string, config? :AxiosRequestConfig): AxiosPromise
+
+  head(url: string, config? :AxiosRequestConfig): AxiosPromise
+
+  post(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  put(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+
+  patch(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise
+}
