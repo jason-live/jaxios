a1d0b079ed2991c6596b132a35c5ee2760d267d6
Jason <348842258@qq.com>
2020-01-20 18:43:56 +0800

    feat: :sparkles: demo 编写

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosTransformer } from '../../src/index'
 import qs from 'qs'
 
-axios.defaults.headers.common['test2'] = 123
+// axios.defaults.headers.common['test2'] = 123
 
 // axios({
 //   url: '/config/post',
@@ -16,7 +16,26 @@ axios.defaults.headers.common['test2'] = 123
 //   console.log(res.data)
 // })
 
-axios({
+// axios({
+//   transformRequest: [(function(data) {
+//     return qs.stringify(data)
+//   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+//   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
+//     if (typeof data === 'object') {
+//       data.b = 2
+//     }
+//     return data
+//   }],
+//   url: '/config/post',
+//   method: 'post',
+//   data: {
+//     a: 1
+//   },
+// }).then((res) => {
+//   console.log(res.data)
+// })
+
+const instance = axios.create({
   transformRequest: [(function(data) {
     return qs.stringify(data)
   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
@@ -25,12 +44,15 @@ axios({
       data.b = 2
     }
     return data
-  }],
+  }]
+})
+
+instance({
   url: '/config/post',
   method: 'post',
   data: {
     a: 1
-  },
+  }
 }).then((res) => {
   console.log(res.data)
 })
