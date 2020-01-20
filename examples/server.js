342b0d4100c4efd3d33c659ed83ea5a4331acf8c
Jason <348842258@qq.com>
2020-01-20 14:53:43 +0800

    feat: :sparkles: demo编写

--- examples/server.js
+++ examples/server.js
@@ -78,49 +78,53 @@ const router = express.Router()
 //   }, 3000)
 // })
 
-router.get('/extend/get', function(req, res) {
-  res.json({
-    msg: 'hello world'
-  })
-})
+// router.get('/extend/get', function(req, res) {
+//   res.json({
+//     msg: 'hello world'
+//   })
+// })
+//
+// router.options('/extend/options', function(req, res) {
+//   res.end()
+// })
+//
+// router.delete('/extend/delete', function(req, res) {
+//   res.end()
+// })
+//
+// router.head('/extend/head', function(req, res) {
+//   res.end()
+// })
+//
+// router.post('/extend/post', function(req, res) {
+//   res.json(req.body)
+// })
+//
+// router.put('/extend/put', function(req, res) {
+//   res.json(req.body)
+// })
+//
+// router.patch('/extend/patch', function(req, res) {
+//   res.json(req.body)
+// })
+//
+// router.get('/extend/user', function(req, res) {
+//   res.json({
+//     code: 0,
+//     message: 'ok',
+//     result: {
+//       name: 'jack',
+//       age: 18
+//     }
+//   })
+// })
+//
+// router.get('/interceptor/get', function(req, res) {
+//   res.end('hello')
+// })
 
-router.options('/extend/options', function(req, res) {
-  res.end()
-})
-
-router.delete('/extend/delete', function(req, res) {
-  res.end()
-})
-
-router.head('/extend/head', function(req, res) {
-  res.end()
-})
-
-router.post('/extend/post', function(req, res) {
+router.post('/config/post', function(req, res) {
   res.json(req.body)
-})
-
-router.put('/extend/put', function(req, res) {
-  res.json(req.body)
-})
-
-router.patch('/extend/patch', function(req, res) {
-  res.json(req.body)
-})
-
-router.get('/extend/user', function(req, res) {
-  res.json({
-    code: 0,
-    message: 'ok',
-    result: {
-      name: 'jack',
-      age: 18
-    }
-  })
-})
-
-router.get('/interceptor/get', function(req, res) {
-  res.end('hello')
 })
 
 
