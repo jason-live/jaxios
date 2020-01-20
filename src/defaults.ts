a506de5c1bb67f5831035efe64e273b713331c23
Jason <348842258@qq.com>
2020-01-20 13:17:22 +0800

    feat: :sparkles: 默认配置编写

---
+++ src/defaults.ts
@@ -0,0 +1,27 @@
+import { AxiosRequestConfig } from './types'
+
+const defaults: AxiosRequestConfig = {
+  timeout: 0,
+  method: 'get',
+  headers: {
+    common: {
+      Accept: 'application/json, text/plain, */*'
+    }
+  }
+}
+
+const methodWithoutData = ['get', 'head', 'options', 'delete']
+
+methodWithoutData.forEach(method => {
+  defaults.headers[method] = {}
+})
+
+const methodWithData = ['put', 'post', 'patch']
+
+methodWithData.forEach(method => {
+  defaults.headers[method] = {
+    'Content-Type': 'applicaton/x-www-form-urlencoded'
+  }
+})
+
+export default defaults
